9382a08da11467c2aa6be151d6052ae175618ff0
Jared Ephemeral <jared.menrod@gmail.com>
2022-07-08 09:10:52 -0600

    Agregado metodo burbuja

--- intro-javascript/Actividad 5/Problemas.js
+++ intro-javascript/Actividad 5/Problemas.js
@@ -17,8 +17,16 @@ while(numeroCinco < 5){
 console.log(arregloVacio);
 
 /*Problema 3*/
-var arreglo2 = [1,4,6,10,22,55,46,2,5,0];
-arreglo2.sort(function(a,b){return a-b});
-for(i=0; i <= arreglo2.length-1; i++){
-    console.log(arreglo2[i]);
+var arregloPrincipal = [1,4,6,10,22,55,46,2,5,0];
+let numeroAyuda = 0;
+//arreglo2.sort(function(a,b){return a-b});
+for(i=0; i <= arregloPrincipal.length-1; i++){
+    for( j = i+1; j <= arregloPrincipal.length -1; j++){
+        if(arregloPrincipal[i] > arregloPrincipal[j]){
+            numeroAyuda = arregloPrincipal[i];
+            arregloPrincipal[i] = arregloPrincipal[j];
+            arregloPrincipal[j] = numeroAyuda;
+        }
+    }
 }
+console.log(arregloPrincipal);
