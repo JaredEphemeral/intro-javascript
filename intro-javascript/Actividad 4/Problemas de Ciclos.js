96118c5532811bdff7957168fe5959854de73484
Jared Ephemeral <jared.menrod@gmail.com>
2022-07-07 18:06:56 -0600

    quickfix

--- intro-javascript/Actividad 4/Problemas de Ciclos.js
+++ intro-javascript/Actividad 4/Problemas de Ciclos.js
@@ -56,20 +56,19 @@ let controlDoWhile = true
 do{
     do{
         numeroRNG1 = Math.floor(Math.random() * 10);
-    }while(numeroRNG1 >= 10 && numeroRNG1 < 0);
+    }while(numeroRNG1 >= 10 || numeroRNG1 < 0);
 
     do{
         numeroRNG2 = Math.floor(Math.random() * 1000);
-    }while(numeroRNG2 > 999 && numeroRNG2 < 0);
+    }while(numeroRNG2 > 999 || numeroRNG2 < 0);
 
     total = numeroRNG1 * numeroRNG2;
-
-        respuestaUsuario = Number(prompt("¿Cuanto da la multiplicación de " + numeroRNG1 + " por " + numeroRNG2 + "?"));
-        if(respuestaUsuario == total){
-            controlDoWhile = false;
-            alert("Ganaste!");
-        }
-        else{
-            alert("Intentalo de nuevo.");
-        }
+    respuestaUsuario = Number(prompt("¿Cuanto da la multiplicación de " + numeroRNG1 + " por " + numeroRNG2 + "?"));
+    if(respuestaUsuario == total){
+        controlDoWhile = false;
+        alert("Ganaste!");
+    }
+    else{
+        alert("Intentalo de nuevo.");
+    }
 }while(controlDoWhile);
