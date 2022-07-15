f641f744885b3b9c7384e05f8e3ce51cc7a9c4e5
Jared Ephemeral <jared.menrod@gmail.com>
2022-07-14 20:03:37 -0600

    .

---
+++ intro-javascript/Practica 1/script.js
@@ -0,0 +1,20 @@
+let resultado = document.getElementById("resultado");
+let calculo = 0;
+function numero(numero){
+    calculo = 0;
+    resultado.textContent = numero;
+}
+
+function operacion(id){
+    switch(id){
+        case 1: //suma
+            break;
+        case 2: //resta
+            break;
+        case 3: //multiplicación
+            break;
+        case 4: //divición
+            break;
+    }
+
+}
